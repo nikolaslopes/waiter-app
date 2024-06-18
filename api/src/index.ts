4cc061a71731f1829aeee5842b3c5fac00de4460
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-18 00:26:38 -0400

    chore: add express json parser

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 
+import { router } from "./router";
+
 mongoose
 	.connect("mongodb://localhost:27017")
 	.then(() => {
 		const app = express();
-
 		const port = 8080;
+
+		app.use(express.json());
+		app.use(router);
+
 		app.listen(port, () => {
 			console.log(`🔥 Server is running on http://localhost:${port}`);
 		});
