17ebeba3711efccf2afdeb107b6e6f2c99a3e261
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-18 11:47:09 -0400

    feat: create static path to show images

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import express from "express";
 import mongoose from "mongoose";
 import "express-async-errors";
@@ -11,6 +12,10 @@ mongoose
 		const app = express();
 		const port = 8080;
 
+		app.use(
+			"/uploads",
+			express.static(path.resolve(__dirname, "..", "uploads")),
+		);
 		app.use(express.json());
 		app.use(router);
 		app.use(errorHandler);
