9e606cc139eca661261c3b0588d59b4ec09f3af6
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-18 11:47:31 -0400

    feat: add multer to handle with uploads

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,22 +1,31 @@
+import path from "node:path";
+
 import { Router } from "express";
+import multer from "multer";
 
 import CategoryController from "./app/controllers/CategoryController";
+import ProductController from "./app/controllers/ProductController";
 
 export const router = Router();
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination(request, file, callback) {
+			callback(null, path.resolve(__dirname, "..", "uploads"));
+		},
+		filename(request, file, callback) {
+			callback(null, `${Date.now()}-${file.originalname}`);
+		},
+	}),
+});
 
 // Categories
 router.get("/categories", CategoryController.index);
 router.post("/categories", CategoryController.store);
 
-// List products
-router.get("/products", (req, res) => {
-	res.send("OK");
-});
-
-// Create product
-router.post("/products", (req, res) => {
-	res.send("OK");
-});
+// Products
+router.get("/products", ProductController.index);
+router.post("/products", upload.single("image"), ProductController.store);
 
 // Get products by category
 router.get("/categories/:id/products", (req, res) => {
