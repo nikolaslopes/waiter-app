cab4b6b10b3c227027ae072ce93320ccc21d6c81
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-23 18:50:53 -0400

    feat: create categories list

---
+++ mobile/src/components/Categories/index.tsx
@@ -0,0 +1,30 @@
+import { FlatList } from "react-native";
+
+import { Text } from "../Text";
+
+import { Category, Icon } from "./styles";
+
+import { categories } from "@/src/mocks/categories";
+
+export function Categories() {
+	return (
+		<FlatList
+			horizontal
+			showsHorizontalScrollIndicator={false}
+			contentContainerStyle={{ paddingRight: 24 }}
+			data={categories}
+			keyExtractor={(category) => category._id}
+			renderItem={({ item: category }) => (
+				<Category>
+					<Icon>
+						<Text>{category.icon}</Text>
+					</Icon>
+
+					<Text size={14} weight="600">
+						{category.name}
+					</Text>
+				</Category>
+			)}
+		/>
+	);
+}
