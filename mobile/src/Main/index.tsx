b55fab976ee9e85c6c186922d454606bb05be7c1
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-24 10:36:56 -0400

    feat: add `Main` screen body

--- mobile/src/Main/index.tsx
+++ mobile/src/Main/index.tsx
@@ -1,11 +1,36 @@
+import { Button } from "../components/Button";
+import { Categories } from "../components/Categories";
 import { Header } from "../components/Header";
+import { Menu } from "../components/Menu";
 
-import { Container } from "./styles";
+import {
+	CategoriesContainer,
+	Container,
+	Footer,
+	FooterContainer,
+	MenuContainer,
+} from "./styles";
 
 export function Main() {
 	return (
-		<Container>
-			<Header />
-		</Container>
+		<>
+			<Container>
+				<Header />
+
+				<CategoriesContainer>
+					<Categories />
+				</CategoriesContainer>
+
+				<MenuContainer>
+					<Menu />
+				</MenuContainer>
+			</Container>
+
+			<Footer>
+				<FooterContainer>
+					<Button onPress={() => alert("aa")}>Novo pedido</Button>
+				</FooterContainer>
+			</Footer>
+		</>
 	);
 }
--- mobile/src/Main/styles.ts
+++ mobile/src/Main/styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components/native";
 import { Platform, StatusBar } from "react-native";
+import styled from "styled-components/native";
 
 const isAndroid = Platform.OS === "android";
 
@@ -7,4 +7,25 @@ console.log({ isAndroid, currentHeight: StatusBar.currentHeight });
 
 export const Container = styled.SafeAreaView`
   margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : "0"};
+  flex: 1;
+  background: #FAFAFA;
+`;
+
+export const CategoriesContainer = styled.View`
+  height: 73px;
+  margin-top: 34px;
+`;
+
+export const MenuContainer = styled.View`
+  height: 50px;
+  flex: 1;
+`;
+
+export const Footer = styled.View`
+  min-height: 110px;
+  background: #FFF;
+  padding: 16px 24px;
+`;
+
+export const FooterContainer = styled.SafeAreaView`
 `;
