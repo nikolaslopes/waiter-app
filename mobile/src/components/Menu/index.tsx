6059e516ab2e6e9a6e15358e0ada755f2a1e4ae6
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-06-26 21:19:55 -0400

    feat: add product modal in menu

--- mobile/src/components/Menu/index.tsx
+++ mobile/src/components/Menu/index.tsx
@@ -3,7 +3,9 @@ import { FlatList, TouchableOpacity } from "react-native";
 import { Text } from "../Text";
 
 import { formatCurrency } from "@/src/utils/formatCurrency";
+import { useState } from "react";
 import { PlusCircle } from "../Icons/PlusCircle";
+import { ProductModal } from "../ProductModal";
 import {
 	AddToCartButton,
 	Divider,
@@ -13,36 +15,44 @@ import {
 } from "./styles";
 
 export function Menu() {
+	const [isProductModalVisible, setIsProductModalVisible] = useState(false);
+
 	return (
-		<FlatList
-			data={products}
-			style={{ marginTop: 32 }}
-			contentContainerStyle={{ paddingHorizontal: 24 }}
-			ItemSeparatorComponent={Divider}
-			keyExtractor={(product) => product._id}
-			renderItem={({ item: product }) => (
-				<Product>
-					<ProductImage
-						source={{
-							uri: `http://192.168.1.12:8080/uploads/${product.imagePath}`,
-						}}
-					/>
+		<>
+			<ProductModal
+				visible={isProductModalVisible}
+				onClose={() => setIsProductModalVisible(false)}
+			/>
+			<FlatList
+				data={products}
+				style={{ marginTop: 32 }}
+				contentContainerStyle={{ paddingHorizontal: 24 }}
+				ItemSeparatorComponent={Divider}
+				keyExtractor={(product) => product._id}
+				renderItem={({ item: product }) => (
+					<Product onPress={() => setIsProductModalVisible(true)}>
+						<ProductImage
+							source={{
+								uri: `http://192.168.1.12:8080/uploads/${product.imagePath}`,
+							}}
+						/>
 
-					<ProductDetails>
-						<Text weight="600">{product.name}</Text>
-						<Text size={14} color="#666666" style={{ marginVertical: 8 }}>
-							{product.description}
-						</Text>
-						<Text weight="600" size={14}>
-							{formatCurrency(product.price)}
-						</Text>
-					</ProductDetails>
+						<ProductDetails>
+							<Text weight="600">{product.name}</Text>
+							<Text size={14} color="#666666" style={{ marginVertical: 8 }}>
+								{product.description}
+							</Text>
+							<Text weight="600" size={14}>
+								{formatCurrency(product.price)}
+							</Text>
+						</ProductDetails>
 
-					<AddToCartButton>
-						<PlusCircle />
-					</AddToCartButton>
-				</Product>
-			)}
-		/>
+						<AddToCartButton>
+							<PlusCircle />
+						</AddToCartButton>
+					</Product>
+				)}
+			/>
+		</>
 	);
 }
