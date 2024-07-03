312aaac43177309bff77765188119cfb5413908d
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-07-03 18:31:44 -0400

    chore: add categories and products mocks

--- mobile/src/mocks/products.ts
+++ mobile/src/mocks/products.ts
@@ -1,39 +1,68 @@
-const products = [
-  {
-    _id: '6372e040f52e37ef85fe2c5e',
-    name: 'Pizza quatro queijos',
-    description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1668472896991-quatro-queijos.png',
-    price: 40,
-    ingredients: [
-      {
-        name: 'Mussarela',
-        icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c5f'
-      },
-      {
-        name: 'Parmesão',
-        icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c60'
-      },
-      {
-        name: 'Gouda',
-        icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c61'
-      },
-      {
-        name: 'Brie',
-        icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c62'
-      }
-    ],
-  },
-  {
-    _id: '6372e276a381106c0f854cb3',
-    name: 'Coca cola',
-    description: 'Coca cola lata geladinha topzera',
-    imagePath: '1668473462705-coca-cola.png',
-    price: 7,
-    ingredients: [],
-  },
+export const products = [
+	{
+		_id: "6674aabab845f7d7c71ccccd",
+		name: "Suco de laranja",
+		description: "Suco de laranja gelado mil grau pra relaxar",
+		imagePath: "1718921914226-suco-de-laranja.png",
+		price: 6,
+		ingredients: [],
+		category: "6670ffc88b6c88379dad07ee",
+		__v: 0,
+	},
+	{
+		_id: "6674aabab845f7d7c71sssssccccd",
+		name: "Suco de toranja",
+		description: "Suco de laranja gelado mil grau pra relaxar",
+		imagePath: "1718921914226-suco-de-laranja.png",
+		price: 6,
+		ingredients: [
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "2382362322483843232323",
+			},
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "23823624112318321111231231312843",
+			},
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "238236248323232111123232843",
+			},
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "23823624323223132283843232323",
+			},
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "23111823624832323321111231231312843",
+			},
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "2382362432321111223832323232843",
+			},
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "2382311112483843232323",
+			},
+			{
+				name: "Laranja",
+				icon: "🍊",
+				_id: "2382363212132248321111231231312843",
+			},
+			{
+				name: "pika",
+				icon: "🍊",
+				_id: "2382362323224832323232843",
+			},
+		],
+		category: "6670ffc88b6c88379dad07ee",
+		__v: 0,
+	},
 ];
