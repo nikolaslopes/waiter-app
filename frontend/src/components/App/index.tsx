9a4f5f02908c469b9034f7bf15500756820447a8
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-07-09 14:07:37 -0400

    feat: add toastify in app

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import "react-toastify/dist/ReactToastify.css";
 
 import { Header } from "../Header";
 
 import { GlobalStyles } from "../../assets/styles/GlobalStyles";
 import { defaultTheme } from "../../assets/styles/themes/default";
 import { Orders } from "../Orders";
+import { ToastContainer } from "react-toastify";
 
 export function App() {
 	return (
@@ -12,6 +14,7 @@ export function App() {
 			<GlobalStyles />
 			<Header />
 			<Orders />
+			<ToastContainer position="bottom-center" />
 		</ThemeProvider>
 	);
 }
